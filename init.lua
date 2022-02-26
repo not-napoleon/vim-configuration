cf5637363fdcf6d1351f863408f413f46af3d156
Mark Tozzi <mark.tozzi@gmail.com>
2022-02-26 17:08:55 -0500

    Vimwiki, put plugins.lua in the right place

--- init.lua
+++ init.lua
@@ -1,4 +1,17 @@
+-- Generic Config
+vim.opt.termguicolors = true
+
+-- Plugin Config
 require("plugins")
 
-vim.opt.termguicolors = true
 require("feline").setup()
+
+vim.g.vimwiki_list = {
+	{
+		name = 'Personal Wiki',
+		path = '~/personalwiki',
+		syntax = 'markdown',
+		ext = '.wiki',
+	}
+}
+
