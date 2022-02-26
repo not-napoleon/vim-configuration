b89edea06f83a87c1c1c43b2c4eda2cb5057f49e
Mark Tozzi <mark.tozzi@gmail.com>
2022-02-26 17:19:03 -0500

    just copy over plugins that still seem relevant

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -16,5 +16,44 @@ return require('packer').startup(function()
     end
   }
 
+
+-- Movements, gestures, and commands{{{2
+  use 'easymotion/vim-easymotion'                -- Fast in document movement
+  use 'AndrewRadev/sideways.vim'                 -- Move items around in lists
+  use 'tomtom/tcomment_vim'                      -- Block commenting
+  use 'tpope/vim-surround'                       -- Handle paired markers
+  use 'tpope/vim-unimpaired'                     -- Paired short-cuts with braces
+
+-- Interface Mods {{{2
+  use 'godlygeek/tabular'                        -- Align text as needed
+  use 'haya14busa/incsearch.vim'                 -- Better incremental search
+  use 'lfv89/vim-interestingwords'               -- Arbitrary highlights
+
+-- Colorschemes {{{2
+  use 'altercation/vim-colors-solarized'         -- Solarized color scheme
+  use 'blueshirts/darcula'
+  use 'freeo/vim-kalisi'
+  use 'jscappini/material.vim'
+  use 'michalbachowski/vim-wombat256mod'
+  use 'romainl/Apprentice'
+  use 'vim-scripts/Perfect-Dark'
+  use 'vim-scripts/moria'
+  use 'xero/blaquemagick.vim'
+  use 'xero/sourcerer.vim'
+
+-- External Systems {{{2
+
+-- Wiki Tools {{{2
   use 'vimwiki/vimwiki'
+
+
+-- Completion {{{2
+  use 'not-napoleon/vim-byline'                  -- Sign your work
+  use 'tpope/vim-abolish'                        -- Better abbreviations
+
+-- Meta {{{2
+  use 'tpope/vim-repeat'                         -- Enable dot repeat for plugins
+
+-- Other {{{2
+
 end)
